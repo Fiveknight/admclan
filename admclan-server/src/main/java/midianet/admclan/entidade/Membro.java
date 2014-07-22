8dfff7777c2b71bcc2c4736db2791d6f1ef5ed3d
midianet <midianet@gmail.com>
2014-07-22 21:51:18 +0000

    criação do projeto

---
+++ admclan-server/src/main/java/midianet/admclan/entidade/Membro.java
@@ -0,0 +1,18 @@
+package midianet.admclan.entidade;
+
+import java.util.Date;
+
+public class Membro {
+	
+	private Long    id;
+	private String  nome;
+	private String  apelido;
+	private Date    dataNascimento;
+	private Date    dataCadastro;
+	private Integer status;
+	private Integer nivelCV;
+	private Integer experiencia;
+	private Cargo  cargo;
+	private String  observacao;
+
+}
